2f9dbdc36f4fbf3c2bdfe9cba2d3fd73f48f89a2
nryzhikh <nikita.ryzhikh@outlook.com>
2023-04-21 23:46:23 +0400

    Make playround fumction

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function getRandomChoice() {
+function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) { return "rock";
     } else if (randomNumber === 1 ) {return "paper"
     } else {return "scissors"
  }
 };
-console.log(getRandomChoice());
+function playRound(playerSelection, computerSelection) {
+    if ( 
+    (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") ||
+    (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") ||
+    (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper")
+    ) {
+        return "You Won! " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelection.toLowerCase() === computerSelection) {
+        return "Draw";
+    } else  {
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+};
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
